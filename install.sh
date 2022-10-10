036f59042551e34c0fb9904936746079ad8bcaf6
Xanthorrhizol <xanthorrhizol@proton.me>
2022-10-11 00:35:11 +0900

    F: groovy

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ done
 echo "install plugins and language servers"
 vim -c "PlugUpdate | PlugUpgrade | PlugInstall | q | q"
 vim -c "CocUpdate"
-vim -c "CocInstall coc-sh coc-rust-analyzer coc-clangd coc-markdownlint coc-tsserver coc-eslint coc-json coc-docker"
+vim -c "CocInstall coc-sh coc-rust-analyzer coc-clangd coc-markdownlint coc-tsserver coc-eslint coc-json coc-docker coc-groovy"
 echo "done"
 
 echo "do you want to setup ctags? [y/N]"
